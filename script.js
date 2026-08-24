4f8fa6ff5e33ecc56303427c106077dd609aab10
raniely <ranielyysilvaa@gmail.com>
2026-08-24 20:53:12 -0300

    atualizar

---
+++ script.js
@@ -0,0 +1,12 @@
+// para criar uma variavel
+
+var nome;
+const nome=" fulano";
+
+var algo=10
+
+//para acessar algum documento da pagina
+
+document.getElementById("teste").addEventListener("click", function(){
+    console.log("Clicado")
+})
