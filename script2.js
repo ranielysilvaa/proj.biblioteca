3bc36800c531d1b91ebb23f33003508da52e21aa
raniely <ranielyysilvaa@gmail.com>
2026-08-25 19:22:10 -0300

    atualização

---
+++ script2.js
@@ -0,0 +1,15 @@
+let button = document.getElementById("test")
+
+
+button.addEventListener("click", ()=>{
+    button.innerHTML= "aperte"
+    button.style.backgroundColor="pink"
+})
+
+
+let jumbo = document.querySelector("#jumbo")
+
+
+button.addEventListener("click",()=>{
+    jumbo.src="hamburgue.png"
+})
